08805fe860fed1a3ceec5be88843842ecc74be47
yuomatsu <07yuya08@gmail.com>
2022-11-30 08:40:12 +0900

    ローディング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // ポケモンデータ取得
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">
+    { loading ? (
+        <h1>ロード中・・・</h1>
+    ) : (
+        <h1>ポケモンデータを取得しました。</h1>
+    )}
+</div>
 }
 
 export default App;
