f7bd6c8665a1c336fe9f18ce308a97f238abfff6
yuomatsu <07yuya08@gmail.com>
2022-11-29 21:26:07 +0900

    全データ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
-  return <div className="App"></div>
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // ポケモンデータ取得
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
