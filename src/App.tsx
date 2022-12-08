79c0b5513201e7bfa388db25229e1b3e69818034
yuomatsu <07yuya08@gmail.com>
2022-12-09 00:57:49 +0900

    詳細なポケモンデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,59 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
-function App() {
+type data = {
+  count: number,
+  next: string,
+  previous: string,
+  results: result
+}
+
+// type results = {
+//   results: result[]
+// }
+
+type result = {
+  name: string,
+  url: string
+}
+
+const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // ポケモンデータ取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      let results = (JSON.parse(JSON.stringify(res))).results;
+
+      // 各ポケモンの詳細データを取得
+      loadPokemon(results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App">
-    { loading ? (
+  const loadPokemon = (data: result[]) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    // console.log(_pokemonData);
+  };
+
+  return (
+    <div className="App">
+      {loading ? (
         <h1>ロード中・・・</h1>
-    ) : (
+      ) : (
         <h1>ポケモンデータを取得しました。</h1>
-    )}
-</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,4 +1,26 @@
+type data = {
+  count: number,
+  next: string,
+  previous: string,
+  results: string[]
+}
+type results = {
+  results: result[]
+}
+
+type result = {
+  name: string,
+  url: string
+}
 export const getAllPokemon = (url: string) => {
+  return new Promise<string[]>((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
+  });
+}
+
+export const getPokemon = (url: string) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
