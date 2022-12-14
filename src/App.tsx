d6f1f4a6dcce25ba4f9481de61bcfbc8351cc9cf
yuomatsu <07yuya08@gmail.com>
2022-12-14 20:45:14 +0900

    カード作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,63 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 type data = {
-  count: number,
-  next: string,
-  previous: string,
-  results: result
-}
+  count: number;
+  next: string;
+  previous: string;
+  results: result;
+};
 
-// type results = {
-//   results: result[]
-// }
+type pokemonData = {
+  abilities: Object[];
+  base_experience: number;
+  forms: Object[];
+  game_indices: Object[];
+  height: number;
+  held_items: Object[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: Object[];
+  name: string;
+  order: number;
+  past_types: [];
+  species: {
+    name: string;
+    url: string;
+  };
+  sprites: {
+    back_default: string;
+    back_female: string;
+    back_shiny: string;
+    back_shiny_female: string;
+    front_default: string;
+    other: Object;
+    versions: Object;
+  };
+  stats: Object[];
+  types: Object[];
+  weight: number;
+};
 
 type result = {
-  name: string,
-  url: string
-}
+  name: string;
+  url: string;
+};
 
 const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // ポケモンデータ取得
       let res = await getAllPokemon(initialURL);
-      let results = (JSON.parse(JSON.stringify(res))).results;
+      // ??? この一行を記載せず、res.resultsとするとエラーが出る
+      let results = JSON.parse(JSON.stringify(res)).results;
 
       // 各ポケモンの詳細データを取得
       loadPokemon(results);
@@ -35,25 +66,32 @@ const App = () => {
     fetchPokemonData();
   }, []);
 
-  const loadPokemon = (data: result[]) => {
-    let _pokemonData = Promise.all(
+  const loadPokemon = async (data: result[]) => {
+    let _pokemonData: any = await Promise.all(
       data.map((pokemon) => {
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
     );
-    // console.log(_pokemonData);
+    setPokemonData(_pokemonData);
   };
+  console.log(pokemonData);
 
   return (
     <div className="App">
       {loading ? (
         <h1>ロード中・・・</h1>
       ) : (
-        <h1>ポケモンデータを取得しました。</h1>
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon ,i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        </>
       )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Card.css";
+
+type type = {
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+const Card = ({ pokemon }: any) => {
+  console.log(pokemon);
+  return (
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>タイプ</div>
+        {pokemon.types.map((type: type) => {
+          return (
+            <div>
+              <div>
+                <span className="typeNmae">{type.type.name}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">重さ：{pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">高さ：{pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">
+            アビリティ：{pokemon.abilities[0].ability.name}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
